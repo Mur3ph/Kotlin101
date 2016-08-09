842b498d930d4110fe03417ea9aaafbaaa34bb34
Mur3ph <paulmurphymainstreet@gmail.com>
2016-08-09 18:13:46 +0100

    Added some functionality with hashmaps

--- src/Collections/List/MyArrayList.kt
+++ src/Collections/List/MyArrayList.kt
@@ -7,6 +7,7 @@ fun main(args: Array<String>) {
     val list = arrayListOf(1, 2, 3)
     list.forEach { println(it) }
 
+    //English to French translation
     val list2 = arrayListOf(
             Pair("Hello", "Bonjour / Bonsoir"),
             Pair("Good bye", "Au revoir"),
---
+++ src/Collections/Map/MyHashMap.kt
@@ -0,0 +1,31 @@
+package Collections.Map
+
+/**
+ * Created by Paul on 8/9/2016.
+ */
+
+// English to Chinese translation
+fun main(args: Array<String>) {
+    val dictionary = hashMapOf(
+            "Hello" to "Ni hao",
+            "Thank you" to "Xie Xie Ni",
+            "See ya" to "Zai Jian "
+    )
+
+    println("Hello in chinese: ${dictionary["Hello"]}" )
+
+    //This is called multi declaration, where you assign each paired key and value to seperate variables
+    for ((myKey, myValue) in dictionary)
+        println("Key in $myKey has a value of $myValue")
+
+    for(myKey in dictionary.keys)
+        println("Key in '$myKey' with a value of '${dictionary[myKey]}'")
+
+    println("Now, looping using the entrySet")
+
+    for(n in dictionary.entries)
+        println("Key ${n.key} has a value of ${n.value}")
+
+
+
+}
