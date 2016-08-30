e13870582719a1023d2447e7e8cb11316fa0f1f1
Mur3ph <paulmurphymainstreet@gmail.com>
2016-08-30 13:04:21 +0100

    Working with a Data Class and using the copy function

--- src/ie/murph/objects/application/App.kt
+++ src/ie/murph/objects/application/App.kt
@@ -1,6 +1,7 @@
 package ie.murph.objects.application
 
 import ie.murph.objects.constructors.Primary
+import ie.murph.objects.copy_data_class.Footballer
 
 /**
  * Created by Paul on 8/29/2016.
@@ -8,10 +9,21 @@ import ie.murph.objects.constructors.Primary
 
 fun main(args : Array<String>)
 {
+    println(" *** My Constructors *** ")
     var primaryConstruct1 = Primary("Joe Bloggs", 32)
     primaryConstruct1.sayName()
 
     var primaryConstruct2 = Primary("Jane Doe")
     primaryConstruct2.sayName()
+    println("")
+
+    println(" *** My Data Class using Copy functon *** ")
+    val footballerOne = Footballer("Roy Keane", "Man. Utd")
+    footballerOne.printPlayerWithClub()
+
+    //Using the 'copy' function
+    val footballerTwo = footballerOne.copy(footballersName = "Paul Scholes")
+    footballerTwo.printPlayerWithClub()
+    println("")
 
 }
---
+++ src/ie/murph/objects/copy_data_class/MyCopyDataClass.kt
@@ -0,0 +1,13 @@
+package ie.murph.objects.copy_data_class
+
+/**
+ * Created by Paul on 8/30/2016.
+ */
+
+data class Footballer(val footballersName: String, val club: String)
+{
+    public fun printPlayerWithClub()
+    {
+        println("$footballersName $club")
+    }
+}
